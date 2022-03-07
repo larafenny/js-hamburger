3b2eb726d6a876a3177b6e3e5a749d4615760eba
Lara Filippone <lara.filippone@gmail.com>
2022-03-07 16:12:43 +0100

    created const click

--- main.js
+++ main.js
@@ -2,3 +2,6 @@
 
 // test per verificare se ho collegato correttamente fiile js
 console.log ('JS ok');
+
+// creo costante click che si riferisce alla classe fas fa-bars
+const click = document.getElementByClass('fas fa-bars');
